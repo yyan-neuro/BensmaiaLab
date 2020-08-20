6e5477b2e6ba8684546aaf7465fe9f6df00b11e2
yyan-neuro <63325710+yyan-neuro@users.noreply.github.com>
2020-08-20 16:20:51 -0500

    Update NLPCA_Final.m

--- HandKinematics/NLPCA_Final.m
+++ HandKinematics/NLPCA_Final.m
@@ -1,5 +1,3 @@
-%MatList_DS = {allGraspMat_BD_DS, allGraspMat_BP_DS, allGraspMat_DM_DS, allGraspMat_EH_DS, allGraspMat_JB_DS, allGraspMat_JF_DS, allGraspMat_LO_DS, allGraspMat_YY_DS};
-%MatIndList_DS = {allGraspMat_BD_DS_ind, allGraspMat_BP_DS_ind, allGraspMat_DM_DS_ind, allGraspMat_EH_DS_ind, allGraspMat_JB_DS_ind, allGraspMat_JF_DS_ind, allGraspMat_LO_DS_ind, allGraspMat_YY_DS_ind};
 
 
 nlpca_netlist = {}; nlpca_nwlist = {};
