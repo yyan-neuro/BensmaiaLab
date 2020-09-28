0c09e0f26a101cd95cb93baa35ea1706a399ece7
yyan-neuro <63325710+yyan-neuro@users.noreply.github.com>
2020-09-28 18:01:16 -0500

    Update ClassAccuracy_DS_Isomap_Calc.m

--- HandKinematics/Analysis/ClassAccuracy_DS_Isomap_Calc.m
+++ HandKinematics/Analysis/ClassAccuracy_DS_Isomap_Calc.m
@@ -1,8 +1,14 @@
 function acc_vec = ClassAccuracy_DS_Isomap_Calc(graspMat,ds_ind,precon,Y)
 
+% Calculate classification performance with progressively smaller dimensions
+% Input:
+%   graspMat: movement data matrix. time x joint angle
+%   ds_ind: movement data information.
+%   precon: number of seconds before contact. Determine the time stamp of the hand posture used in classificaiton.
+% Output: 
+%   acc_vec: classification performance on the test data
+
 acc_vec = []; PCNo = 29;
-
-
 graspMat_Isomap = Y.coords{29}; graspMat_Isomap = graspMat_Isomap.';
 
 
