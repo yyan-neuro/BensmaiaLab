88db9715c3805b92a79791a97a93a232f151ca93
yyan-neuro <63325710+yyan-neuro@users.noreply.github.com>
2020-09-28 15:29:31 -0500

    Update ClassAccuracy_DS_Isomap_Calc.m

--- HandKinematics/Analysis/ClassAccuracy_DS_Isomap_Calc.m
+++ HandKinematics/Analysis/ClassAccuracy_DS_Isomap_Calc.m
@@ -2,14 +2,7 @@ function acc_vec = ClassAccuracy_DS_Isomap_Calc(graspMat,ds_ind,precon,Y)
 
 acc_vec = []; PCNo = 29;
 
-%{
-landmark_index = randperm(size(graspMat,1)); landmark_index = landmark_index(1:50);
-landmark_index = sort(landmark_index);
-options.dims = 1:29; options.display = false; options.landmarks = landmark_index ;
 
-D = Isomap_L2_distance(graspMat.',graspMat.',1);
-[Y, R, ~,~] = IsomapII_returnD(D, 'k', 29, options);
-%}
 graspMat_Isomap = Y.coords{29}; graspMat_Isomap = graspMat_Isomap.';
 
 
@@ -19,10 +12,7 @@ for i = 1:PCNo
     
     [tmat,vmat,~,~] = getClassMat_ds(ds_ind,graspMat_Isomap,precon,false);
     
-    %{
-    tmat_pc = tmat(:,1:end-30+i);
-    vmat_pc = vmat(:,1:end-30+i);
-    %}
+
     
     tmat_pc = tmat(:,end-i:end-1);
     vmat_pc = vmat(:,end-i:end-1);
@@ -38,11 +28,5 @@ for i = 1:PCNo
     
 end
 
-%{
-plot(1:PCNo,acc_vec,'LineWidth',1.5);
-xlabel('PC Number');
-ylabel('Accuracy / percent');
-ylim([0,1]);
-hold on;
-%}
+
 end
